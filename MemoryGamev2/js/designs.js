e6ce0293148b16b25e4b37784e9fdef4f154d49b
Karol Szelążek <charles.sz@protonmail.com>
2019-01-20 15:33:36 +0100

    Created algorithm for signs generation. Added some CSS for neater look.]

--- MemoryGamev2/js/designs.js
+++ MemoryGamev2/js/designs.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
 
-  for (var row = 0; row < 5; row++) {
-    $("table").append("<tr class='row'></tr>")
-    for (var index = 0; index < 5; index++) {
-      $("table").append("<td class='cell'></td>")
+  const listOfSigns = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb", "fa fa-automobile", "fa fa-file-text", "fa fa-gear", "fa fa-bullhorn"];
+  let signID = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11];
+  let row, index, id, holder;
+
+  for (index = 0; index < 25; index++) {
+    id = -1;
+    while (id < 0 || id > signID.length - 1) id = Math.floor(Math.random() * 100);
+    holder = signID[id];
+    signID[id] = signID[0];
+    signID[0] = holder;
+  }
+
+  id = 0;
+
+  for (row = 1; row <= 5; row++) {
+    $("table").append("<tr class='row'></tr>");
+    for (index = 1; index <= 5; index++) {
+      if (row == 3 && index == 3) $("table").append("<td class='center " + listOfSigns[0] + "'></td>");
+      else {
+        $("table").append("<td class='" + listOfSigns[signID[id]] + "'></td>");
+        id++;
+      }
     }
   }
 
-})
+});
